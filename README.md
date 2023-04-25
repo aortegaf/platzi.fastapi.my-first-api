7093945e57b8f1e42a79ac06ce81f6109c82429a
aortegaf <andres.ortega@puntored.co>
2023-04-25 07:54:47 -0500

    GET method implementation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# platzi.fastapi.my-first-api
+# My first API with FastApi
+
+To install requirements:
+
+    pip install -r requirements.txt
--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.title = "My FastAPI"
 app.version = "1.0"
 
-@app.get('/')
+movies = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "year": 2009
+    },
+    {
+        "id": 2, 
+        "title": "Avengers: Infinity War",
+        "year": 2018
+    }
+]
+
+@app.get('/', tags=["home"])
 def message():
-    return "Hello World!"
+    return HTMLResponse('<h1>This is my first API with FastAPI</h1>')
+
+@app.get('/movies', tags=["movies"])
+def get_movies():
+    return movies
+
+
+
+
+
