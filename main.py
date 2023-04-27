2dca59523916821bf953ca55383a18f56cb07ea5
aortegaf <andres.ortega@puntored.co>
2023-04-27 08:40:02 -0500

    response_model added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Path, Query
 from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
+from typing import List
 
 app = FastAPI()
 
@@ -46,37 +47,41 @@ movies = [
 def message():
     return HTMLResponse('<h1>This is my first API with FastAPI</h1>')
 
-@app.get('/movies', tags=["movies"])
-def get_movies():
+@app.get('/movies', tags=["movies"], response_model=List[Movie])
+def get_movies() -> List[Movie]:
     return JSONResponse(content=movies)
 
-@app.get('/movies/', tags=["movies"])
-def get_movies_by_year(year: int = Query(ge=2000)):
+@app.get('/movies/', tags=["movies"], response_model=List[Movie])
+def get_movies_by_year(year: int = Query(ge=2000)) -> List[Movie]:
+    data = []
     for m in movies:
         if(m["year"] == year):
-            return JSONResponse(content=m)
+            data.append(m)
+    return JSONResponse(content=data)
 
-@app.get('/movies/{id}', tags=["movies"])
-def get_movie(id: int = Path(ge=1, le=10)):
+@app.get('/movies/{id}', tags=["movies"], response_model=Movie)
+def get_movie(id: int = Path(ge=1, le=10)) -> Movie:
+    data = []
     for m in movies:
         if(m["id"] == id):
-            return JSONResponse(content=m)
+            data.append(m)
+    return JSONResponse(content=data)
         
-@app.post('/movies', tags=["movies"])
-def add_movie(movie: Movie):
+@app.post('/movies', tags=["movies"], response_model=dict)
+def add_movie(movie: Movie) -> dict:
     movies.append(movie)
     return JSONResponse(content={"message": "Movie succesfully created!"})
 
-@app.put('/movies', tags=["movies"])
-def update_movie(id: int, movie: Movie):
+@app.put('/movies', tags=["movies"], response_model=dict)
+def update_movie(id: int, movie: Movie) -> dict:
     for m in movies:
         if(m["id"] == id):
             m["title"] = movie.title
             m["year"] = movie.year
     return JSONResponse(content={"message": "Movie succesfully updated!"})
 
-@app.delete('/movies', tags=["movies"])
-def delete_movie(id: int):
+@app.delete('/movies', tags=["movies"], response_model=dict)
+def delete_movie(id: int) -> dict:
     for m in movies:
         if(m["id"] == id):
             movies.remove(m)
